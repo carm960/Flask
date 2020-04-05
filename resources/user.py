86b8e1ffec53a64d864dc6483cd5c10ff369fdda
mikyzz <cvivenzo@tiscali.it>
2020-04-05 13:46:36 +0200

    Fisrt add flask api

---
+++ resources/user.py
@@ -0,0 +1,28 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from models.user import UserModel
+
+class UserRegister(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+                        type=str,
+                        required=True,
+                        help="Questo campo non può essere vuoto!"
+                        )
+    parser.add_argument('password',
+                        type=str,
+                        required=True,
+                        help="Questo campo non può essere vuoto!"
+                        )
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+
+        if UserModel.find_by_username(data['username']):
+            return {"message": "Utente già esistente."}, 400
+
+        user = UserModel(**data)
+        user.save_to_db()
+
+        return {"message": "User created successfully."}, 201
